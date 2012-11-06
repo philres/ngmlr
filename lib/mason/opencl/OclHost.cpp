a7843716517a760d9949517976cb99ea77d2c99a
philres <philipp.rescheneder@univie.ac.at>
2012-11-06 13:22:21 +0100

    Print OpenCL driver version.

--- lib/mason/opencl/OclHost.cpp
+++ lib/mason/opencl/OclHost.cpp
@@ -10,8 +10,6 @@
 #include <string.h>
 #include <pthread.h>
 
-
-
 pthread_mutex_t mutext_next_sub_block;
 
 #undef module_name
@@ -31,13 +29,13 @@ enum PLATFORM {
 PLATFORM platform;
 
 OclHost::OclHost() :
-	devType(CL_DEVICE_TYPE_GPU) {
+		devType(CL_DEVICE_TYPE_GPU) {
 	OclHost(0, 0, 1);
 
 }
 
 OclHost::OclHost(int const device_type, int gpu_id, int const cpu_cores) :
-	devType(device_type), maxGlobalMem(0), maxLocalMem(0) {
+		devType(device_type), maxGlobalMem(0), maxLocalMem(0) {
 	//	if (!isGPU()) {
 	//			gpu_id = 0;
 	//	}
@@ -51,9 +49,12 @@ OclHost::OclHost(int const device_type, int gpu_id, int const cpu_cores) :
 	oclDevice = getDevice(gpu_id);
 
 	char device_string[1024];
+	char driver_string[1024];
 	clGetDeviceInfo(oclDevice, CL_DEVICE_NAME, sizeof(device_string), &device_string, NULL);
+	clGetDeviceInfo(oclDevice, CL_DRIVER_VERSION, sizeof(driver_string), &driver_string, NULL);
 	//	if (isGPU()) {
-	Log.Message("Device %d selected (%s)", gpu_id, device_string);
+	Log.Message("Device %s selected (ID: %d, Driver: %s)", device_string,gpu_id, driver_string);
+
 	//	} else {
 	//		Log.Message("Device %d selected using %d core(s) (%s)", gpu_id, getDeviceInfoInt(CL_DEVICE_MAX_COMPUTE_UNITS), device_string);
 	//	}
@@ -114,7 +115,10 @@ cl_platform_id getPlatformID(char const * const platformName) {
 				if (strcasestr(clPlatformName, platformName) != 0) {
 					Log.Message("Selecting OpenCl platform: %s", clPlatformName);
 					ciErrNum = clGetPlatformInfo(clPlatformID[i], CL_PLATFORM_VERSION, 1024, &clPlatformName, NULL);
-					Log.Message("Version: %s", clPlatformName);
+					Log.Message("Platform: %s", clPlatformName);
+					//ciErrNum = clGetDeviceInfo(oclDevice, CL_DRIVER_VERSION, 1024, &clPlatformName, NULL);
+					//ciErrNum = clGetPlatformInfo(clPlatformID[i], CL_DRIVER_VERSION, 1024, &clPlatformName, NULL);
+					//Log.Message("Driver: %s", clPlatformName);
 					return clPlatformID[i];
 				}
 			} else {
@@ -339,7 +343,6 @@ cl_program OclHost::setUpProgram(char const * const oclSwScore, std::string buil
 	//char const * additional_options_nv = " -cl-nv-verbose -cl-fast-relaxed-math";
 	//char * additional_options = 0;
 
-
 	size_t program_length = strlen(oclSwScore);
 	if (strcasestr(clPlatformName, "NVIDIA") != 0) {
 		buildOptions += " -D __NVIDIA__ -cl-nv-verbose -cl-fast-relaxed-math";
@@ -403,20 +406,20 @@ int OclHost::getThreadPerMulti() {
 		int id = 10 * revision + getDeviceInfoLong(CL_DEVICE_COMPUTE_CAPABILITY_MINOR_NV);
 
 		switch (id) {
-			case 10:
-				return 768;
-			case 11:
-				return 768;
-			case 12:
-				return 1024;
-			case 13:
-				return 1024;
-			case 20:
-				return 1536;
-			case 21:
-				return 1536;
-			default:
-				return 1536;
+		case 10:
+			return 768;
+		case 11:
+			return 768;
+		case 12:
+			return 1024;
+		case 13:
+			return 1024;
+		case 20:
+			return 1536;
+		case 21:
+			return 1536;
+		default:
+			return 1536;
 		}
 	} else {
 		return 256;
@@ -479,99 +482,99 @@ void OclHost::waitForDevice() {
 
 char * OclHost::print_cl_errstring(cl_int err) {
 	switch (err) {
-		case CL_SUCCESS:
-			return strdup("Success!");
-		case CL_DEVICE_NOT_FOUND:
-			return strdup("Device not found.");
-		case CL_DEVICE_NOT_AVAILABLE:
-			return strdup("Device not available");
-		case CL_COMPILER_NOT_AVAILABLE:
-			return strdup("Compiler not available");
-		case CL_MEM_OBJECT_ALLOCATION_FAILURE:
-			return strdup("Memory object allocation failure");
-		case CL_OUT_OF_RESOURCES:
-			return strdup("Out of resources");
-		case CL_OUT_OF_HOST_MEMORY:
-			return strdup("Out of host memory");
-		case CL_PROFILING_INFO_NOT_AVAILABLE:
-			return strdup("Profiling information not available");
-		case CL_MEM_COPY_OVERLAP:
-			return strdup("Memory copy overlap");
-		case CL_IMAGE_FORMAT_MISMATCH:
-			return strdup("Image format mismatch");
-		case CL_IMAGE_FORMAT_NOT_SUPPORTED:
-			return strdup("Image format not supported");
-		case CL_BUILD_PROGRAM_FAILURE:
-			return strdup("Program build failure");
-		case CL_MAP_FAILURE:
-			return strdup("Map failure");
-		case CL_INVALID_VALUE:
-			return strdup("Invalid value");
-		case CL_INVALID_DEVICE_TYPE:
-			return strdup("Invalid device type");
-		case CL_INVALID_PLATFORM:
-			return strdup("Invalid platform");
-		case CL_INVALID_DEVICE:
-			return strdup("Invalid device");
-		case CL_INVALID_CONTEXT:
-			return strdup("Invalid context");
-		case CL_INVALID_QUEUE_PROPERTIES:
-			return strdup("Invalid queue properties");
-		case CL_INVALID_COMMAND_QUEUE:
-			return strdup("Invalid command queue");
-		case CL_INVALID_HOST_PTR:
-			return strdup("Invalid host pointer");
-		case CL_INVALID_MEM_OBJECT:
-			return strdup("Invalid memory object");
-		case CL_INVALID_IMAGE_FORMAT_DESCRIPTOR:
-			return strdup("Invalid image format descriptor");
-		case CL_INVALID_IMAGE_SIZE:
-			return strdup("Invalid image size");
-		case CL_INVALID_SAMPLER:
-			return strdup("Invalid sampler");
-		case CL_INVALID_BINARY:
-			return strdup("Invalid binary");
-		case CL_INVALID_BUILD_OPTIONS:
-			return strdup("Invalid build options");
-		case CL_INVALID_PROGRAM:
-			return strdup("Invalid program");
-		case CL_INVALID_PROGRAM_EXECUTABLE:
-			return strdup("Invalid program executable");
-		case CL_INVALID_KERNEL_NAME:
-			return strdup("Invalid kernel name");
-		case CL_INVALID_KERNEL_DEFINITION:
-			return strdup("Invalid kernel definition");
-		case CL_INVALID_KERNEL:
-			return strdup("Invalid kernel");
-		case CL_INVALID_ARG_INDEX:
-			return strdup("Invalid argument index");
-		case CL_INVALID_ARG_VALUE:
-			return strdup("Invalid argument value");
-		case CL_INVALID_ARG_SIZE:
-			return strdup("Invalid argument size");
-		case CL_INVALID_KERNEL_ARGS:
-			return strdup("Invalid kernel arguments");
-		case CL_INVALID_WORK_DIMENSION:
-			return strdup("Invalid work dimension");
-		case CL_INVALID_WORK_GROUP_SIZE:
-			return strdup("Invalid work group size");
-		case CL_INVALID_WORK_ITEM_SIZE:
-			return strdup("Invalid work item size");
-		case CL_INVALID_GLOBAL_OFFSET:
-			return strdup("Invalid global offset");
-		case CL_INVALID_EVENT_WAIT_LIST:
-			return strdup("Invalid event wait list");
-		case CL_INVALID_EVENT:
-			return strdup("Invalid event");
-		case CL_INVALID_OPERATION:
-			return strdup("Invalid operation");
-		case CL_INVALID_GL_OBJECT:
-			return strdup("Invalid OpenGL object");
-		case CL_INVALID_BUFFER_SIZE:
-			return strdup("Invalid buffer size");
-		case CL_INVALID_MIP_LEVEL:
-			return strdup("Invalid mip-map level");
-		default:
-			return strdup("Unknown");
+	case CL_SUCCESS:
+		return strdup("Success!");
+	case CL_DEVICE_NOT_FOUND:
+		return strdup("Device not found.");
+	case CL_DEVICE_NOT_AVAILABLE:
+		return strdup("Device not available");
+	case CL_COMPILER_NOT_AVAILABLE:
+		return strdup("Compiler not available");
+	case CL_MEM_OBJECT_ALLOCATION_FAILURE:
+		return strdup("Memory object allocation failure");
+	case CL_OUT_OF_RESOURCES:
+		return strdup("Out of resources");
+	case CL_OUT_OF_HOST_MEMORY:
+		return strdup("Out of host memory");
+	case CL_PROFILING_INFO_NOT_AVAILABLE:
+		return strdup("Profiling information not available");
+	case CL_MEM_COPY_OVERLAP:
+		return strdup("Memory copy overlap");
+	case CL_IMAGE_FORMAT_MISMATCH:
+		return strdup("Image format mismatch");
+	case CL_IMAGE_FORMAT_NOT_SUPPORTED:
+		return strdup("Image format not supported");
+	case CL_BUILD_PROGRAM_FAILURE:
+		return strdup("Program build failure");
+	case CL_MAP_FAILURE:
+		return strdup("Map failure");
+	case CL_INVALID_VALUE:
+		return strdup("Invalid value");
+	case CL_INVALID_DEVICE_TYPE:
+		return strdup("Invalid device type");
+	case CL_INVALID_PLATFORM:
+		return strdup("Invalid platform");
+	case CL_INVALID_DEVICE:
+		return strdup("Invalid device");
+	case CL_INVALID_CONTEXT:
+		return strdup("Invalid context");
+	case CL_INVALID_QUEUE_PROPERTIES:
+		return strdup("Invalid queue properties");
+	case CL_INVALID_COMMAND_QUEUE:
+		return strdup("Invalid command queue");
+	case CL_INVALID_HOST_PTR:
+		return strdup("Invalid host pointer");
+	case CL_INVALID_MEM_OBJECT:
+		return strdup("Invalid memory object");
+	case CL_INVALID_IMAGE_FORMAT_DESCRIPTOR:
+		return strdup("Invalid image format descriptor");
+	case CL_INVALID_IMAGE_SIZE:
+		return strdup("Invalid image size");
+	case CL_INVALID_SAMPLER:
+		return strdup("Invalid sampler");
+	case CL_INVALID_BINARY:
+		return strdup("Invalid binary");
+	case CL_INVALID_BUILD_OPTIONS:
+		return strdup("Invalid build options");
+	case CL_INVALID_PROGRAM:
+		return strdup("Invalid program");
+	case CL_INVALID_PROGRAM_EXECUTABLE:
+		return strdup("Invalid program executable");
+	case CL_INVALID_KERNEL_NAME:
+		return strdup("Invalid kernel name");
+	case CL_INVALID_KERNEL_DEFINITION:
+		return strdup("Invalid kernel definition");
+	case CL_INVALID_KERNEL:
+		return strdup("Invalid kernel");
+	case CL_INVALID_ARG_INDEX:
+		return strdup("Invalid argument index");
+	case CL_INVALID_ARG_VALUE:
+		return strdup("Invalid argument value");
+	case CL_INVALID_ARG_SIZE:
+		return strdup("Invalid argument size");
+	case CL_INVALID_KERNEL_ARGS:
+		return strdup("Invalid kernel arguments");
+	case CL_INVALID_WORK_DIMENSION:
+		return strdup("Invalid work dimension");
+	case CL_INVALID_WORK_GROUP_SIZE:
+		return strdup("Invalid work group size");
+	case CL_INVALID_WORK_ITEM_SIZE:
+		return strdup("Invalid work item size");
+	case CL_INVALID_GLOBAL_OFFSET:
+		return strdup("Invalid global offset");
+	case CL_INVALID_EVENT_WAIT_LIST:
+		return strdup("Invalid event wait list");
+	case CL_INVALID_EVENT:
+		return strdup("Invalid event");
+	case CL_INVALID_OPERATION:
+		return strdup("Invalid operation");
+	case CL_INVALID_GL_OBJECT:
+		return strdup("Invalid OpenGL object");
+	case CL_INVALID_BUFFER_SIZE:
+		return strdup("Invalid buffer size");
+	case CL_INVALID_MIP_LEVEL:
+		return strdup("Invalid mip-map level");
+	default:
+		return strdup("Unknown");
 	}
 }
