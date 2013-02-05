9d1354be54916fc62e339e5e611d941e28a44ed4
philres <philipp.rescheneder@univie.ac.at>
2013-02-05 11:01:28 +0100

    Bamtools finally added.

---
+++ lib/bamtools/src/toolkit/bamtools_version.h
@@ -0,0 +1,20 @@
+// ***************************************************************************
+// bamtools_version.h.in (c) 2010 Derek Barnett
+// Marth Lab, Department of Biology, Boston College
+// ---------------------------------------------------------------------------
+// Last modified: 19 November 2010 (DB)
+// ---------------------------------------------------------------------------
+// Provides version information for the BamTools toolkit.
+// ***************************************************************************
+
+#ifndef BAMTOOLS_VERSION_H
+#define BAMTOOLS_VERSION_H
+
+// CMake uses this file as a template to generate "bamtools_version.h".
+// These constants are defined to match the variables set in the build system. 
+#define BAMTOOLS_VERSION_MAJOR 2
+#define BAMTOOLS_VERSION_MINOR 2
+#define BAMTOOLS_VERSION_BUILD 3
+
+#endif // BAMTOOLS_VERSION_H
+
