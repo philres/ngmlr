b0c5a80d6c3b3e11a60ff724a9778512e33a4960
philres <philipp.rescheneder@univie.ac.at>
2013-07-23 12:57:14 +0200

    ngm-utils: Don't write "(null)" to outpt file if no quality values are present in input.

--- utils/writer/FastqWriter.h
+++ utils/writer/FastqWriter.h
@@ -21,9 +21,17 @@ public:
 
 	virtual void writeRead(MappedRead * read) {
 		Print("@%s\n", read->name);
-		Print("%s\n", read->Seq);
+		if (read->Seq != 0) {
+			Print("%s\n", read->Seq);
+		} else {
+			Print("\n");
+		}
 		Print("+\n");
-		Print("%s\n", read->qlty);
+		if (read->qlty != 0) {
+			Print("%s\n", read->qlty);
+		} else {
+			Print("\n");
+		}
 		//data << "@" << read->name << std::endl << read->Seq << std::endl << "+" << std::endl << read->qlty << std::endl;
 		Flush();
 	}
