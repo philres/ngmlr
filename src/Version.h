c28b2826ef2c28415e4b891773eaacb225305472
philres <philipp.rescheneder@univie.ac.at>
2013-10-21 18:02:49 +0200

    New Version + debug mode

--- src/Version.h
+++ src/Version.h
@@ -1,9 +1,9 @@
 #ifndef VERSION_H
 #define VERSION_H
  
-#define VERSION_MAJOR 0
-#define VERSION_MINOR 4
-#define VERSION_BUILD 8
+#define VERSION_MAJOR "0"
+#define VERSION_MINOR "4"
+#define VERSION_BUILD "9-pre"
 
 #endif // VERSION_H
 
