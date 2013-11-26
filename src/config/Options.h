d6ed03a15c422e27e05f160a9aa7e7018500b91e
philres <philipp.rescheneder@univie.ac.at>
2013-11-26 14:51:48 +0100

    Added -d

--- src/config/Options.h
+++ src/config/Options.h
@@ -10,7 +10,7 @@
 
 #include <getopt.h>
 
-static char const * getopt_short = "c:o:q:r:t:gs:m:f:k:pleI:X:i:n:R:C:b1:2:";
+static char const * getopt_short = "c:o:q:r:t:gs:m:f:k:pleI:X:i:n:R:C:b1:2:d:";
 
 //Here '-' has to be used and not '_'. When querying the Config the '-' has to be replaced by '_'.
 //When passing parameters trough the config file '-' and '_' can both be used.
