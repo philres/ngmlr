3354d95d0b480dbfcba869c30527ff14998d10a0
philres <philipp.rescheneder@univie.ac.at>
2017-03-27 20:39:47 +0200

    Test case 3 update

--- test/test_3.sh
+++ test/test_3.sh
@@ -8,11 +8,14 @@ PARAMETER=" --skip-write -x pacbio --no-progress -t 4 -R 0.01 "
 
 echo "Test: $NAME"
 
+bin/${BIN}/ngmlr $PARAMETER -r test/data/test_3/reference.fasta.gz -q test/data/test_3/read.fa.gz 2> /dev/null | samtools view -Sb - 2> /dev/null | bedtools bamtobed | sort > test/data/test_3/expected.bed
 for i in {1..5}
 do
 	bin/${BIN}/ngmlr $PARAMETER -r test/data/test_3/reference.fasta.gz -q test/data/test_3/read.fa.gz 2> /dev/null | samtools view -Sb - 2> /dev/null | bedtools bamtobed | sort > test/data/test_3/found.bed
 	diff test/data/test_3/expected.bed test/data/test_3/found.bed || exit 1
 	rm test/data/test_3/found.bed
 done
+
+rm test/data/test_3/expected.bed
 
 echo "Success"
