c662143c195816bbd04a6f245d3f17587c5d3d95
philres <philipp.rescheneder@univie.ac.at>
2017-05-15 11:48:16 +0200

    Travis test update

--- test/test_travis.sh
+++ test/test_travis.sh
@@ -2,8 +2,8 @@
 
 # Read name length test removed: max. name length in BAM is 254, replaced with test_5.sh
 #bash test/test_1.sh
-bash test/test_2.sh
-bash test/test_3.sh
-bash test/test_4.sh
-bash test/test_5.sh
-bash test/test_6.sh
+test/test_2.sh
+test/test_3.sh
+test/test_4.sh
+test/test_5.sh
+test/test_6.sh
